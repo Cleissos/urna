d66bc6a7dbdab9b2ba24cd4578401958c938f472
Cleisson <mc4220762@gmail.com>
2024-11-13 22:17:57 -0300

    Alteração de imagens, mais uma função inserida

--- script.js
+++ script.js
@@ -1,126 +1,143 @@
-function botao1(){
+function botao1() {
     document.getElementById('campo').value += '1';
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function botao2(){
+function botao2() {
     document.getElementById('campo').value += '2';
     var b1 = document.getElementById('b1');
+    verificaCandidato()
     b1.play();
 }
 
-function botao3(){
+function botao3() {
     document.getElementById('campo').value += '3';
     var b1 = document.getElementById('b1');
+    verificaCandidato()
     b1.play();
 }
 
-function botao4(){
+function botao4() {
     document.getElementById('campo').value += '4';
     var b1 = document.getElementById('b1');
+    verificaCandidato()
     b1.play();
 }
 
-function botao5(){
+function botao5() {
     document.getElementById('campo').value += '5';
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function botao6(){
+function botao6() {
     document.getElementById('campo').value += '6';
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function botao7(){
+function botao7() {
     document.getElementById('campo').value += '7';
     var b1 = document.getElementById('b1');
+    verificaCandidato()
     b1.play();
 }
 
-function botao8(){
+function botao8() {
     document.getElementById('campo').value += '8';
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function botao9(){
+function botao9() {
     document.getElementById('campo').value += '9';
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function botao0(){
+function botao0() {
     document.getElementById('campo').value += '0';
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function branco(){
+function branco() {
     var b1 = document.getElementById('b1');
     b1.play();
 }
 
-function corrige(){
+function corrige() {
 
     var b1 = document.getElementById('b1');
     b1.play();
-        var texto = document.getElementById('campo').value;
-    
-    if(texto == '17'){
+    var texto = document.getElementById('campo').value;
+
+    if (texto == '17') {
         var imagem = document.getElementById('bolsonaro');
         var imagem2 = document.getElementById('justica');
         imagem2.style.display = 'block';
         imagem.style.display = 'none'
-    }else if(texto == '13'){
+    } else if (texto == '13') {
         var imagem = document.getElementById('lula');
         var imagem2 = document.getElementById('justica')
         imagem2.style.display = 'block';
         imagem.style.display = 'none'
-    }else if(texto == '12'){
+    } else if (texto == '12') {
         var imagem = document.getElementById('ciro');
         var imagem2 = document.getElementById('justica')
         imagem2.style.display = 'block';
         imagem.style.display = 'none'
-    }else if(texto == '' ){
+    } else if(texto == '14'){
+        var imagem = document.getElementById('padreKelmon');
+        var imagem2 = document.getElementById('justica')
+        imagem2.style.display = 'block';
+        imagem.style.display = 'none'
+    }
+    else if (texto == '') {
         var imagem2 = document.getElementById('justica');
         var imagem = document.getElementById('votoInvalido');
         imagem2.style.display = 'block';
-        imagem.style.display = 'none'; 
+        imagem.style.display = 'none';
     }
-    else{
+    else {
         var imagem = document.getElementById('votoInvalido');
         var imagem2 = document.getElementById('justica')
         imagem2.style.display = 'block';
-        imagem.style.display = 'none'; 
+        imagem.style.display = 'none';
     }
 
-    document.getElementById('campo').value = '';    
+    document.getElementById('campo').value = '';
 }
 
-function confirma(){
-    var texto = document.getElementById('campo').value;
-    
+
+
+
+function verificaCandidato() {
     // Primeiro, ocultar todas as imagens
     var imagens = document.querySelectorAll('#div01 img');
-    imagens.forEach(function(img) {
+    imagens.forEach(function (img) {
         img.style.display = 'none';
     });
+
+    var texto = document.getElementById('campo').value;
 
     // Verificar o valor digitado e exibir a imagem correspondente
     if (texto == '17') {
         var imagem = document.getElementById('bolsonaro');
-        imagem.setAttribute('src', '../img/Bolsonaro.png');
+        imagem.setAttribute('src', 'img/cachorro1.png');
         imagem.style.display = 'block';
-    }else if(texto == '13'){
+    } else if (texto == '13') {
         var imagem = document.getElementById('lula');
-        imagem.setAttribute('src','../img/Lula.png');
+        imagem.setAttribute('src', 'img/gato2.png');
         imagem.style.display = 'block';
-    }else if(texto == '12'){
+    } else if (texto == '12') {
         var imagem = document.getElementById('ciro');
-        imagem.setAttribute('src','../img/CIRO.png');
+        imagem.setAttribute('src', 'img/cachorro2.png');
+        imagem.style.display = 'block';
+    }else if(texto == '14'){
+        var imagem = document.getElementById('padreKelmon');
+        imagem.setAttribute('src', 'img/gat-presidente.png');
         imagem.style.display = 'block';
     }
     else {
@@ -131,20 +148,34 @@ function confirma(){
         imagem.style.display = 'block';
         imagem2.style.display = 'none';
     }
+}
+
+function confirma() {
 
     //Tocar som de confirmação
     var somConfirma = document.getElementById('somConfirma');
     somConfirma.play();
 
+    var imagens = document.querySelectorAll('#div01 img');
+    imagens.forEach(function (img) {
+        img.style.display = 'none';
+    });
+
+    var imagem2 =document.getElementById('fim')
+    imagem2.setAttribute('src', '../img/FIM2.png')
+    imagem2.style.display = 'block'
+
     //Essa funçaõ dá um delay de 2s quando votar e apertar o botão confirma, ela volta à imagem original
-    setTimeout(function() {
+    setTimeout(function () {
         // Ocultar a imagem atual
-        imagem.style.display = 'none';
-        
+        //var imagem = document.getElementById('justica')
+        //imagem.style.display = 'none';
+        imagem2.style.display = 'none'
+
         // Exibir a imagem da justiça
         var imagemJustica = document.getElementById('justica');
         imagemJustica.style.display = 'block';
-        
+
         // Limpar o campo de entrada
         document.getElementById('campo').value = '';
     }, 2000); // 2000 milissegundos = 2 segundos de atraso
